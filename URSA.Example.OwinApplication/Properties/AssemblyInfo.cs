f231fde668d087a5025fe4cadcc8a4c355966d7a
Karol Szczepański <alien-mcl@users.noreply.github.com>
2016-01-20 21:25:03 +0100

    Code tweaks.

---
+++ URSA.Example.OwinApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using URSA.Web.Mapping;
+
+[assembly: AssemblyTitle("URSA.Example.OwinApplication")]
+[assembly: AssemblyDescription("URSA OWIN self-hosted example web application.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0ccd1c5a-a161-4291-ac28-d280270531b2")]
+[assembly: Route("api")]
---
+++ URSA.Http/HeaderCollection.IEnumerable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace URSA.Web.Http
+{
+    /// <summary>Represents an HTTP header collection.</summary>
+    public sealed partial class HeaderCollection : IEnumerable<Header>
+    {
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        public IEnumerator<Header> GetEnumerator()
+        {
+            return _headers.Values.GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator()
+        {
+            return _headers.Select(header => new KeyValuePair<string, string>(header.Key, header.Value.Value)).GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
