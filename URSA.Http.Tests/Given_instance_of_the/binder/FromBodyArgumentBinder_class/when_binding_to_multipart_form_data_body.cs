28f0a77b268823b21451909c8df7e536f7d7b04c
Karol Szczepański <alien-mcl@users.noreply.github.com>
2016-05-08 20:59:04 +0200

    Added missing files.

---
+++ URSA.Http.Tests/Given_instance_of_the/binder/FromBodyArgumentBinder_class/when_binding_to_multipart_form_data_body.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using URSA;
+using URSA.Web;
+using URSA.Web.Http;
+using URSA.Web.Http.Mapping;
+using URSA.Web.Http.Testing;
+using URSA.Web.Mapping;
+
+namespace Given_instance_of_the.binder.FromBodyArgumentBinder_class
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class when_binding_to_multipart_form_data_body : ArgumentBinderTest<FromBodyArgumentBinder, FromBodyAttribute, int>
+    {
+        private const string Boundary = "test";
+        private const string Body =
+            "--" + Boundary + "\r\nContent-Disposition: form-data; name=\"operandA\"\r\nContent-Type: text/plain\r\nContent-Length:3\r\n\r\n1\r\n" +
+            "--" + Boundary + "\r\nContent-Disposition: form-data; name=\"operandB\"\r\nContent-Type: text/plain\r\nContent-Length:3\r\n\r\n2\r\n--" + Boundary + "--";
+
+        protected override HttpUrl RequestUrl { get { return (HttpUrl)UrlParser.Parse("http://temp.org/api/test/modulo"); } }
+
+        protected override HttpUrl MethodUrl { get { return (HttpUrl)UrlParser.Parse("http://temp.org/api/test/modulo"); } }
+
+        protected override string MethodName { get { return "PostModulo"; } }
+
+        [TestMethod]
+        public void it_should_call_converter_provider()
+        {
+            Binder.GetArgumentValue(GetContext(Body, "POST", "multipart/form-data", Boundary));
+
+            ConverterProvider.Verify(instance => instance.FindBestInputConverter(It.IsAny<Type>(), It.IsAny<IRequestInfo>(), false), Times.Once);
+        }
+
+        [TestMethod]
+        public void it_should_call_converter()
+        {
+            Binder.GetArgumentValue((ArgumentBindingContext)GetContext(Body, "POST", "multipart/form-data", Boundary));
+
+            Converter.Verify(instance => instance.ConvertTo(It.IsAny<Type>(), It.IsAny<IRequestInfo>()), Times.Once);
+        }
+    }
+}
